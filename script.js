54f03ff40f2bdb71cbbfe0c36c6b49354dcd5245
BenBasic <benjaminmbasic@gmail.com>
2022-06-02 11:53:51 -0400

    Changed direction to instead try grabbing data from ticket master api, currently have obtained a data set of 100 events happening in the Toronto area

--- script.js
+++ script.js
@@ -1,43 +1,8 @@
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-const url = "http://data.streetfoodapp.com/1.1/locations/vancouver/vijs"; // site that doesn’t send Access-Control-*
-fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+let apiKey = "gPYdcAZ3x7dA2mYoq34XovsV4rz8IFvd"
+let ticketMasterURL = "https://app.ticketmaster.com/discovery/v2/events.json?dmaId=527&apikey=" + apiKey + "&size=100";
+fetch(ticketMasterURL) 
 .then(response => response.json())
 .then(contents => console.log(contents))
 .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 
 
-apiKey = "ae6d8ad7-7180-4d04-9a45-cdcf2e70eeff";
-
-function getUVIndex() {
-    let UVIndexURL = "https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/package_show?id=" + apiKey;
-    let testURL = "https://cors-anywhere.herokuapp.com/";
-    fetch(testURL + UVIndexURL, {
-        method: "get",
-    })
-    .then( UVResponse => {
-        return UVResponse.json();
-    })
-    .then( UVData => {
-        console.log(UVData);
-
-    })
-}
-
-getUVIndex();
-
-
-// function getUVIndex() {
-//     UVIndexURL = "http://data.streetfoodapp.com/1.1/locations/vancouver/vijs"
-//     fetch(UVIndexURL, {
-//         method: "get",
-//     })
-//     .then( UVResponse => {
-//         return UVResponse.json();
-//     })
-//     .then( UVData => {
-//         console.log(UVData);
-
-//     })
-// }
-
-// getUVIndex();
