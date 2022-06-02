eb3d93185693db83ed3fc26a57471b062855e032
BenBasic <benjaminmbasic@gmail.com>
2022-06-02 13:33:40 -0400

    Added functionality for the program to grab the date of the events in the api data and compare it with the current date to see if the api data is between the current date and 7 days after the current date, checks if its true or false. I plan to use this functionality for grabbing the lat and lon data if the dateCheck is true, still need to work on that aspect of it

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
 let apiKey = "gPYdcAZ3x7dA2mYoq34XovsV4rz8IFvd"
-let ticketMasterURL = "https://app.ticketmaster.com/discovery/v2/events.json?dmaId=527&apikey=" + apiKey + "&size=100";
-fetch(ticketMasterURL) 
-.then(response => response.json())
-.then(contents => console.log(contents))
-.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+
+
+
+
+let querySize = "50"
+
+// fetch(ticketMasterURL) 
+// .then(response => response.json())
+// .then(data => console.log(data))
+// let dateData = data._embedded.events;
+
+// console.log(dateData)
+
+// .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+
+
+
+function getData() {
+    // Accessing data from the api with the searched city and metric units
+    let ticketMasterURL = "https://app.ticketmaster.com/discovery/v2/events.json?dmaId=527&apikey=" + apiKey + "&size=" + querySize + "&sort=date,asc";
+    fetch(ticketMasterURL, {
+    method: 'GET',
+})
+.then( response => { 
+    // Returns json object for reference
+    return response.json();
+}).then( data => {
+    console.log(data);
+
+    let currentDate = moment().format("YYYY-MM-DD");
+    let startWeekDate = moment().subtract(1, 'days').format("YYYY-MM-DD")
+    let endWeekDate = moment().add(7, 'days').format("YYYY-MM-DD")
+
+    console.log(currentDate)
+
+    let eventData = data._embedded.events;
+    
+    console.log(eventData)
+
+    for (i = 0; i < data._embedded.events.length; i++) {
+        let dateData = data._embedded.events[i].dates.start.localDate;
+        console.log(dateData);
+
+        function checkBetween(date1, date2, date3) {
+            return moment(date1).isBetween(date2, date3);
+        }
+           
+        let dateCheck = checkBetween(dateData, 
+            startWeekDate, endWeekDate);
+        console.log(dateCheck);
+
+    }
+})
+}
+
+getData();
 
 
