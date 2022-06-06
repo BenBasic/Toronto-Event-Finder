22e738516728b43b4b3f83759328f4ecb9ad8a7c
BenBasic <benjaminmbasic@gmail.com>
2022-06-06 00:01:52 -0400

    Fixed error where events that were in the past would show up on the map

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ if ((localStorage = null)) {
 // Assigning the apiKey variable to the API Key needed to access the data set
 let apiKey = "gPYdcAZ3x7dA2mYoq34XovsV4rz8IFvd";
 // Assigning querySize to the amount of events we want to pull from a page in the data set (currently the maximum value for the api is 1000 per page)
-let querySize = "50";
+let querySize = "200";
 
 const mapBoxKey =
 	"pk.eyJ1Ijoienp6YmlhIiwiYSI6ImNsM3ZubXB0djJuc2UzZGw4NHBscnltb3IifQ.DW29ynZDDnPeH6hmtl8O8g"; // set the access token
@@ -75,7 +75,7 @@ function getData() {
 				let cancelledCheck = eventData[i].dates.status.code;
 
 				// If statement to check if dateCheck is true, and if cancelledCheck is equal to onsale
-				if ((dateCheck, cancelledCheck === "onsale")) {
+				if ((dateCheck && cancelledCheck === "onsale")) {
 					// Assigning latData to the value of the events data set latitude
 					let latData = eventData[i]._embedded.venues[0].location.latitude;
 					// Assigning lonData to the value of the events data set longitude
@@ -92,7 +92,7 @@ function getData() {
 					let eventStartDay = eventData[i].dates.start.localDate;
 					// Starting Time of the event
 					let eventStartTime = eventData[i].dates.start.localTime;
-
+					console.log(dateCheck);
 					if (
 						latData &&
 						lonData &&
